c1f97e8d055f4374a2d6fcbe4837e9a04e738049
Utsav Barnwal <123968318+Fiesta11@users.noreply.github.com>
2023-12-28 10:55:22 +0000

    learnt datatypes

---
+++ Js_Basic/1variables.js
@@ -0,0 +1,17 @@
+const account_id = 14453;
+let account_email = "utsav@gmail.com";
+var account_pass = "122324";
+account_city = "Lucknow";
+let account_state
+// account_id = 3; * Not Allowed *
+account_email = "ut@email.com"
+account_pass = "111"
+account_city = "Jaipur"
+
+// console.log(typeof(account_city))
+
+
+// console.log(account_id);
+console.table([account_id ,account_email , account_pass , account_city , account_state]);
+
+// note - prefer not to use var because of issue in block and functional scope
---
+++ Js_Basic/2datatypes.js
@@ -0,0 +1,18 @@
+// "use strict"; // treat all js code as a newer version
+// alert(3+5); //"error" we are using nodeJs not browser
+
+// note - code readability should be high
+// number
+// string
+// boolean
+// null - standalone value
+// undefined 
+// symbol
+// object
+
+let name = "utsav";
+
+console.log(typeof name);
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(3+4);
