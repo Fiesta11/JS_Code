9a967a7079cfbd0e5cfdad461857c44de05709f4
Utsav Barnwal <123968318+Fiesta11@users.noreply.github.com>
2024-01-03 10:08:45 +0000

    numsandmath

---
+++ Js_Basic/8numsandmath.js
@@ -0,0 +1,25 @@
+const score = 400;
+const balance = new Number(500);
+
+// console.log(score);
+// console.log(balance);
+const str = balance.toString();
+// console.log(typeof str);
+// console.log(balance.toFixed(4));
+
+const num = 3343.456;
+// console.log(num.toPrecision(5));
+
+const hundreds = 10000000;
+// console.log(hundreds.toLocaleString('en-IN'))
+// console.log(Math);
+// console.log(Math.abs(-6));
+// console.log(Math.round(4.50));
+// console.log(Math.ceil(5.3));
+// console.log(Math.floor(5.3));
+// console.log(Math.max(3,6,9,10));
+console.log(Math.round(Math.random()*10) + 1);
+const min = 10;
+const max = 20;
+
+console.log(Math.round(Math.random() * (max - min + 1) + min));
